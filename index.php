a6a91aedaaad0ec4272f6a096690b9e02acd1c41
Eduard <dawbevidalgallardo@iesjoanramis.org>
2022-03-06 05:49:41 +0100

    Cambio en el index

--- index.php
+++ index.php
@@ -50,14 +50,6 @@
     </div>
     <br><br>
   </div>
-
   <?php include("./templates/footer.php"); ?>
-
-
-  <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
-
   </body>
 </html>
