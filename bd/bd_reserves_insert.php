a1547d7653a30f2095927431e6637491ce4bf622
Eduard <dawbevidalgallardo@iesjoanramis.org>
2022-04-26 16:27:49 +0200

    Empezamos a intentar gestionar si hay habitaciones disponibles a la hora de hacer una reserva

--- bd/bd_reserves_insert.php
+++ bd/bd_reserves_insert.php
@@ -1,5 +1,5 @@
 <?php
-    
+    include('../config/db_connexio.php');
     if (isset($_POST['submit'])) {
         $idcliente = $_POST['idcliente'];
         $datein = $_POST['datein'];
@@ -12,10 +12,16 @@
         $cantidad_personas = $_POST['cantidad_personas'];
         $noches = (array)date_diff(date_create($datein), date_create($dateout));
         $precio = $th[$tipo]['precio']*$noches['days'];
+        $habitaciones_disponibles = mysqli_query($connexio, "SELECT count(*) FROM 64_habitacio WHERE tipo_habitacion = $tipo");
+        $habitaciones_reservadas = mysqli_query($connexio, "SELECT count(*) FROM `64_reserves_view` WHERE 'ID Tipus Habitacio' = $tipo");
+        if(($habitaciones_disponibles - $habitaciones_reservadas) <= 0) {
+            header("Location: ../forms/form_reserves_insert.php");
+        }
+
     }
     echo "Precio: ".$precio." Data in: ".$datein." Data out: ".$dateout." Tipo: ".$tipo." Pension: ".$pension." Id cliente: ".$idcliente." Id habitacion: ".$idhabitacion." Id estado: ".$idestado."Personas: ".$cantidad_personas." Noches: ".$noches['days'];
 
-    include('../config/db_connexio.php');
+    
     $sql = 
     "INSERT INTO 64_reserves (cliente_id, entrada, salida, habitacion_id, estado_id, noches, precio, pension_id)
                         VALUES ($idcliente, '$datein', '$dateout', $idhabitacion, $idestado, $noches[days], $precio, $pension);";
